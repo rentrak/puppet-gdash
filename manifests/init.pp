af08a8d098f4b6f7c49ff4ac620bf87aac84073e
Tom De Vylder <tom@penumbra.be>
2013-02-19 11:55:47 +0100

    Lintian fixes and code consistency

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,11 @@
 # Class: gdash
 #
-# This module manages gdash
-#
-# Parameters:
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
-# [Remember: No empty lines between comments and class definition]
 class gdash (
-  $graphitehost='127.0.0.1',
-  $gdashroot = '/usr/local/gdash/'
-)
-{
-
-  # I`m expecting the package to install it's content in /usr/local/gdash
-
-
-  file{"${gdashroot}":
-    ensure => directory;
+  $gdashroot    = '/usr/local/gdash',
+  $graphitehost = '127.0.0.1',
+) {
+  file { $gdashroot:
+    ensure => directory,
   }
 
   #package {
@@ -29,41 +13,40 @@ class gdash (
   #    ensure  => '1.3.3-1',
   #    require => Package['rubygem-rack']
   #}
-  package {
-    'rubygem-bundler':
-      ensure => present
+
+  package { 'rubygem-bundler':
+    ensure => present,
   }
+
   #package {
   #  'rubygem-tilt':
   #    ensure => present
   #}
+
   #  package {
   #  'rubygem-rack':
   #    ensure => '1.3.4-1'
   #}
+
   #package {
   #  'rubygem-rack-protection':
   #    ensure  => present,
   #    require => Package['rubygem-rack']
   #}
 
+  package { 'gdash':
+    ensure => present,
+  }
 
-
-
-  package { 'gdash': ensure => present }
   package { 'gdash-bundled-deps':
     ensure => '0.98.14-1',
   }
 
-
-  file {
-    "${gdashroot}/config/gdash.yaml":
-      content => template('gdash/gdash.yaml.erb'),
-      group   => '0',
-      owner   => '0',
-      require => Package['gdash'],
+  file { "${gdashroot}/config/gdash.yaml":
+    content => template('gdash/gdash.yaml.erb'),
+    group   => '0',
+    owner   => '0',
+    require => Package['gdash'],
   }
-
-
 }
 
