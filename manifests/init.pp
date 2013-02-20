5603b3138ffddd1c27a2a2d513d809e7d13bdd16
Tom De Vylder <tom@penumbra.be>
2013-02-20 16:20:54 +0100

    Add missing parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,9 @@
 # Class: gdash
 #
 class gdash (
+  $config_dir        = $::gdash::params::config_dir,
+  $config_group      = $::gdash::params::config_group,
+  $config_user       = $::gdash::params::config_user,
   $graphite_host     = $::gdash::params::graphite_host,
   $log_dir           = $::gdash::params::log_dir,
   $package_deps      = $::gdash::params::package_deps,
@@ -10,6 +13,10 @@ class gdash (
   $template_dir      = $::gdash::params::template_dir,
   $vhost_config_file = $::gdash::params::vhost_config_file,
   $vhost_dir         = $::gdash::params::vhost_dir,
+  $vhost_group       = $::gdash::params::vhost_group,
+  $vhost_serveralias = $::gdash::params::vhost_serveralias,
+  $vhost_servername  = $::gdash::params::vhost_servername,
+  $vhost_user        = $::gdash::params::vhost_user,
   $whisper_dir       = $::gdash::params::whisper_dir,
 ) inherits gdash::params {
   class { 'gdash::install': } ->
